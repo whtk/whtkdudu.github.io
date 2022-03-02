4216696f3c0c181a7becd5bd404c856f4736b453
whtk <whtk210@163.com>
2022-03-02 22:17:53 +0800

    Site updated: 2022-03-02 22:17:52

--- js/weather.js
+++
@@ -1,1 +0,0 @@
-WIDGET={CONFIG:{modules:"01234",background:"5",tmpColor:"FFFFFF",tmpSize:"16",cityColor:"FFFFFF",citySize:"16",aqiColor:"FFFFFF",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"10px 10px 10px 10px",shadow:"0",language:"auto",fixed:"true",vertical:"top",horizontal:"left",left:"100",top:"10",key:"9971f278e4b44401b7aec1fce874d6b4"}};
